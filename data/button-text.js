e76f6814a2ebac4133a06884406f33769d4d3cc5
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-09-01 16:30:28 +0300

    formatted dad joke

---
+++ data/button-text.js
@@ -0,0 +1,15 @@
+const buttonText = [
+    'Ugh.',
+    '🤦🏻‍♂️',
+    'omg dad.',
+    'you are the worst',
+    'seriously',
+    'stop it.',
+    'please stop',
+    'that was the worst one',
+    'I did not expect it',
+    'hahahahahah',
+    'Oooops'
+];
+// default export
+export default buttonText;
